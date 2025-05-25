751d6dacffaf4261080843581d5cc5458ee906bd
edilsonss123 <edilson016@outlook.com>
2025-05-24 21:06:28 -0300

    ajustes docker-compose

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const port = 3002;
+const port = 3000;
 
 const db = new sqlite3.Database("./bible.db", err => {
     if (err) {
